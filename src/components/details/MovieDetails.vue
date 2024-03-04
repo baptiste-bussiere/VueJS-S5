f73a01dbb13a99314bf5e28220a503cad491394e
baptiste buss <contact@baptiste-bussiere.fr>
2024-03-04 16:09:11 +0100

    getMoviesDetail

--- src/components/details/MovieDetails.vue
+++ src/components/details/MovieDetails.vue
@@ -6,28 +6,42 @@
  // MonComposant.vue
 <template>
   <div class="composant movie">
-    <h1>Tous nos films</h1>
+    
     <div v-if="loading">Chargement...</div>
     <div v-else-if="error">Une erreur s'est produite : {{ error }}</div>
-    <div class="cardContainer" v-else >
-      <!-- {{ $route.params.id }} -->
-      <router-link  v-for="item in data" :key="item.id" class="moviecard card" :to="`/movie/details/${item.id}`">
-        <div>{{ item.title }}</div>
-      </router-link>
+    <div class="detailsMovie" v-else >
+      <h1>{{ data.title }}</h1>
+      <div>{{ data.description }}</div>
+      <div>{{ data.releaseDate }}</div>
+      <div>{{ data.duration }}</div>
+
+      Les acteurs 
+
+      <div v-for="actor in data.actor" :key="actor.id">
+        <div>{{ actor }}</div>
+    
+        </div>
+      
+    
     </div>
   </div>
 </template>
 
 <script>
 import callData from '../../hooks/callData';
+  import {useRoute} from "vue-router";
 
 export default {
+
   name: 'Movie',
- 
+
   setup() {
     const { data, loading, error, fetchData } = callData();
-const  movieId = 2
-    fetchData(`http://127.0.0.1:8000/api/movies/${this.$route.params.id}`); 
+    const route = useRoute();
+const id  = route.params.id;
+    
+
+    fetchData(`http://127.0.0.1:8000/api/movies/${id}`); 
 
     return {
       data,
